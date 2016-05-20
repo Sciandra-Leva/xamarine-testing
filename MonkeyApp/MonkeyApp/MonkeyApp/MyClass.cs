83ecf1ccfc7456ae0f65becfbf9b2d34c5961b1b
Lorenzo Sciandra <sciandra@leva.io>
2016-05-20 16:19:53 +0200

    Removed two old projects, started new one. This one builds properly.

---
+++ MonkeyApp/MonkeyApp/MonkeyApp/MyClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MonkeyApp
+{
+
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public int Population { get; set; }
+    }
+
+    public class MonkeysViewModel
+    {
+        public ObservableCollection<Monkey> Monkeys { get; } = new ObservableCollection<Monkey>();
+
+        public async Task GetMonkeyAsync()
+        {
+            try
+            {
+                var client = new HttpClient();
+                var json = await client.GetStringAsync("http://montemagno.com/monkeys.json");
+
+                var items = JsonConvert.DeserializeObject<List<Monkey>>(json);
+
+                foreach (var item in items)
+                {
+                    Monkeys.Add(item);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+    }
+
+
+}
+

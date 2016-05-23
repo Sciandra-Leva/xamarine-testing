d1df58bdef826fa61fbc30f5ff3cd8d8329d2726
Lorenzo Sciandra <sciandra@leva.io>
2016-05-23 15:58:35 +0200

    Added new project, starting working on the MVC

---
+++ XamarinFormsDemoPart4/XamarinFormsDemoPart4/XamarinFormsDemoPart4/HomePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using XamarinFormsDemoPart4.ViewModels;
+
+namespace XamarinFormsDemoPart4
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            BindingContext = new HomeViewModel();
+        }
+    }
+}
---
+++ XamarinFormsDemoPart4/XamarinFormsDemoPart4/XamarinFormsDemoPart4/ViewModels/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using XamarinFormsDemoPart4.Models;
+
+namespace XamarinFormsDemoPart4.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        public TaskModel TaskModel { get; set; }
+
+        public HomeViewModel()
+        {
+            TaskModel = new TaskModel
+            {
+                Title = "Creating UI",
+                Duration = 2
+            };
+
+        }
+    }
+}
